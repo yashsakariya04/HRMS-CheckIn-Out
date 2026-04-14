9eccb8ee01166441dd889349ebee556deecdff2e
Shlok <shlok.infopulsetech@gmail.com>
2026-04-14 10:42:45 +0530

    changes for list project and employee

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -67,8 +67,8 @@ async def get_current_user(
     )
     user = result.scalars().first()
 
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    if not user or not user.is_active:
+        raise HTTPException(status_code=401, detail="User not found or inactive")
 
     return user
 
